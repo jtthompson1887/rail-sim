e1fcb26fa178da16e05f6e468ac9474c74bbcd94
Jordan <8020582+jtthompson1887@users.noreply.github.com>
2026-07-26 06:25:34 +0100

    test: keep Playwright specs out of Jest in worktrees

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   roots: ['<rootDir>/tests'],
-  testPathIgnorePatterns: ['<rootDir>/tests/e2e/'],
+  testPathIgnorePatterns: ['[\\\\/]tests[\\\\/]e2e[\\\\/]'],
   moduleNameMapper: {
     '^phaser$': '<rootDir>/__mocks__/phaser.js',
   },
